89678a63fcdba8d1ea7b6c67766cb274a2d5a728
Andres Martinez <andres.martinez@expertos-ti.com>
2021-03-01 15:10:31 -0600

    Visualize different class numpy array

--- visualize.py
+++ visualize.py
@@ -4,11 +4,41 @@ import numpy as np
 
 def prepare_figure(numpy_array):
     plt.figure()
-    if numpy_array.shape[2] > 1:
+    h, w, c = numpy_array.shape
+    if c == 3:  # RGB numpy array
         plt.imshow(numpy_array)
     else:
-        numpy_array = np.squeeze(numpy_array, axis=-1)
-        plt.imshow(numpy_array, cmap='Greys',  interpolation='nearest')
+        mask = numpy_array[..., 0]
+        class_values = numpy_array[..., 1]
+        image = np.zeros((h, w, 3))
+        for i in range(h):
+            for j in range(w):
+                if mask[(i, j)] > 0:
+                    image[h, w, ...] = get_rgb_color(class_values[(i, j)])
+        plt.imshow(image)
+
+
+def get_rgb_color(class_value):
+    if class_value == 0:
+        return np.array([0, 0, 0])  # black
+    if class_value == 1:
+        return np.array([255, 0, 0])  # red
+    if class_value == 2:
+        return np.array([0, 255, 0])  # green
+    if class_value == 3:
+        return np.array([0, 0, 255])  # blue
+    if class_value == 4:
+        return np.array([255, 255, 0])  # yellow
+    if class_value == 5:
+        return np.array([255, 102, 0])  # orange
+    if class_value == 6:
+        return np.array([0, 102, 255])  # purple
+    if class_value == 7:
+        return np.array([255, 0, 255])  # pink
+    if class_value == 8:
+        return np.array([102, 204, 255])  # light blue
+    if class_value == 9:
+        return np.array([102, 51, 00])  # brown
 
 
 def show_prepared_figures():
